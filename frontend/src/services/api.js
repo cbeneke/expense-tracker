811b5b2d1dd26b503d8a5e877e7c87347ae9bc92
Christian Beneke <christian.beneke@traderepublic.com>
2024-12-18 15:40:27 +0100

    SET API env at runtime

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { env } from './env';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+const API_URL = env.REACT_APP_API_URL;
 
 const api = axios.create({
     baseURL: `${API_URL}/api`,
